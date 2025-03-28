178cda0ec59df3009855c49773edc55374f9d34c
komal <sharmakomal3512@gmail.com>
2025-03-28 16:56:01 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -6,12 +6,19 @@ app.use("/admin",authorize);
 
 
 app.get("/admin/getdata",(req,res)=>{
+
   res.send("data collected successfully");
 });
 
 app.get("/admin/deleteuser",(req,res)=>{
   res.send("deleted user successfully");
 })
+
+//error handling
+app.use("/",(err,req,res,next)=>{
+  if (err)
+  {res.status(500).send("something went wrong ji!!!!")}
+});
 
 
 app.listen(7000, () => {
