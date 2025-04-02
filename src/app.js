eb5f35ef2ae7200c97114cc43d62f762f3265083
komal <sharmakomal3512@gmail.com>
2025-04-02 10:41:59 +0530

    get api using findone

--- src/app.js
+++ src/app.js
@@ -25,16 +25,42 @@ app.post("/signup", async (req, res) => {
 
 app.post("/admin", async (req, res) => {
   const admin = new Admin(req.body);
-    // {
-    // firstName:"aditya",
-    // lastName:"sharma",
-    // password:"aditya123"
-    // }
+  // {
+  // firstName:"aditya",
+  // lastName:"sharma",
+  // password:"aditya123"
+  // }
   try {
     await admin.save();
     res.send("admin data added");
   } catch (err) {
     res.status(400).send("error saving admin data");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const useremail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: useremail });
+
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(404).send("something went wrong");
+  }
+});
+
+//get password using findone
+app.get("/usersdata", async (req, res) => {
+  const passwordadmin=req.body.password
+  try {
+    const admins = await Admin.findOne({password:passwordadmin});
+    res.send(admins);
+  } catch (err) {
+    res.status(404).send("something went wrong");
   }
 });
 
